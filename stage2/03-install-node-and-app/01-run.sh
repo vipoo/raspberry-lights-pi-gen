e1877c8f3f3b9daea4dce58adc5ce324f931d296
Dean Netherton <dnetherton@dius.com.au>
2018-09-28 16:58:29 +1000

    Add and configure lirc package

--- stage2/03-install-node-and-app/01-run.sh
+++ stage2/03-install-node-and-app/01-run.sh
@@ -5,3 +5,7 @@ tar -xzf files/node-v8.12.0-linux-armv6l.tar.gz -C files/
 chown 1000:1000 "files/node-v8.12.0-linux-armv6l" -Rv
 cp -R files/node-v8.12.0-linux-armv6l/* "${ROOTFS_DIR}/usr/local/"
 
+install -v -m 644 files/modules             "${ROOTFS_DIR}/etc/modules"
+install -v -m 644 files/lirc_options.conf   "${ROOTFS_DIR}/etc/lirc/lirc_options.conf"
+install -v -m 644 files/ledstrip.lircd.conf "${ROOTFS_DIR}/etc/lirc/lircd.conf.d/ledstrip.lircd.conf"
+
