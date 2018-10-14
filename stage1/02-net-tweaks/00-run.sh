ebf0e4ca7eb72e838757ac30644b23441a0aba4f
Dean Netherton <dean.netherton@gmail.com>
2018-10-14 08:45:56 +0000

    Configurable hostname

--- stage1/02-net-tweaks/00-run.sh
+++ stage1/02-net-tweaks/00-run.sh
@@ -1,6 +1,12 @@
 #!/bin/bash -e
 
+
 install -m 644 files/ipv6.conf "${ROOTFS_DIR}/etc/modprobe.d/ipv6.conf"
-install -m 644 files/hostname "${ROOTFS_DIR}/etc/hostname"
+
+mkdir -p ./tmp
+echo $RASPI_HOSTNAME > ./tmp/hostname
+install -m 644 ./tmp/hostname "${ROOTFS_DIR}/etc/hostname"
+
+echo "127.0.1.1	 $RASPI_HOSTNAME" >> "${ROOTFS_DIR}/etc/hosts"
 
 ln -sf /dev/null "${ROOTFS_DIR}/etc/systemd/network/99-default.link"
